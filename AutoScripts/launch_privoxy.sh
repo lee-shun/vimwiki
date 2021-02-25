dff6c60203efd2c93e17c201bc4a65764f395710
lee-shun <lee970802@163.com>
2021-02-25 12:12:40 +0800

    update

--- AutoScripts/launch_privoxy.sh
+++
@@ -1,3 +0,0 @@
-gnome-terminal --window -e 'bash -c "service privoxy start"' \
---tab -e 'bash -c " export http_proxy='http://localhost:8118'"' \
---tab -e 'bash -c " export https_proxy='http://localhost:8118'"' \ 
--- AutoScripts/launch_v2ray.sh
+++
@@ -1,3 +0,0 @@
-gnome-terminal --window -e 'bash -c "service v2ray start"' \
---tab -e 'bash -c "service v2ray status"' \
-
